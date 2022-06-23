433b662c4b90464ef0eeda2d2d025c1ec9bea626
GioCri01 <giovanni.crispino94@icloud.com>
2022-06-23 17:37:38 +0200

    aggiunto classe active header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,13 +2,13 @@
     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/DC_Comics_logo.svg/2048px-DC_Comics_logo.svg.png" alt="">
       <ul>
           <li >
-              <a  href="http://127.0.0.1:8000/">home</a>
+              <a class="{{ Route::currentRouteName() === 'home'? 'active': '' }}" href="{{ route("home") }}">home</a>
           </li>
           <li >
-            <a  href="http://127.0.0.1:8000/about">about</a>
+            <a class="{{ Route::currentRouteName() === 'about'? 'active': '' }}" href="{{ route("about") }}">about</a>
         </li>
         <li >
-            <a  href="http://127.0.0.1:8000/shop">shop</a>
+            <a class="{{ Route::currentRouteName() === 'shop'? 'active': '' }}" href="{{ route("shop") }}">shop</a>
         </li>
 
       </ul>
